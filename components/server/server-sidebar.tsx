3712f464cdda2840d486b64d939ab93a34b39e74
fawez9 <hattabifawez99@gmail.com>
2024-04-28 21:36:14 +0100

    verified search server modal

--- components/server/server-sidebar.tsx
+++ components/server/server-sidebar.tsx
@@ -13,17 +13,16 @@ interface ServerSidebarProps {
 }
 
 const iconMap = {
-  [ChannelType.TEXT]: <Hash className="mr-2 h-4 w-4"/>,
-  [ChannelType.AUDIO]: <Mic className="mr-2 h-4 w-4"/>,
-  [ChannelType.VIDEO]: <Video className="mr-2 h-4 w-4"/>
-
+  [ChannelType.TEXT]: <Hash className="mr-2 h-4 w-4" />,
+  [ChannelType.AUDIO]: <Mic className="mr-2 h-4 w-4" />,
+  [ChannelType.VIDEO]: <Video className="mr-2 h-4 w-4" />,
 };
 
 const roleIconMap = {
   [MemberRole.GUEST]: null,
-  [MemberRole.MODERATOR]: <ShieldCheck className="h-4 w-4 mr-2 text-indigo-500"/>,
-  [MemberRole.ADMIN]: <ShieldAlert className="h-4 w-4 mr-2 text-rose-500"/>
-}
+  [MemberRole.MODERATOR]: <ShieldCheck className="h-4 w-4 mr-2 text-indigo-500" />,
+  [MemberRole.ADMIN]: <ShieldAlert className="h-4 w-4 mr-2 text-rose-500" />,
+};
 
 export const ServerSidebar = async ({ serverId }: ServerSidebarProps) => {
   const profile = await currentProfile();
@@ -52,73 +51,61 @@ export const ServerSidebar = async ({ serverId }: ServerSidebarProps) => {
   });
 
   //server channels
-  const textChannels = server?.channels.filter(
-    (channel) => channel.type === ChannelType.TEXT
-  );
-  const audioChannels = server?.channels.filter(
-    (channel) => channel.type === ChannelType.AUDIO
-  );
-  const videoChannels = server?.channels.filter(
-    (channel) => channel.type === ChannelType.VIDEO
-  );
+  const textChannels = server?.channels.filter((channel) => channel.type === ChannelType.TEXT);
+  const audioChannels = server?.channels.filter((channel) => channel.type === ChannelType.AUDIO);
+  const videoChannels = server?.channels.filter((channel) => channel.type === ChannelType.VIDEO);
 
   if (!server) {
     return redirect("/");
   }
   //check if user is in server and get their role
-  const role = server.members.find(
-    (member) => member.profileId === profile.id
-  )?.role;
+  const role = server.members.find((member) => member.profileId === profile.id)?.role;
 
-  const members = server?.members.filter(
-    (member) => member.profileId !== profile.id
-  );
+  const members = server?.members.filter((member) => member.profileId !== profile.id);
   return (
     <div className="flex flex-col h-full text-primary w-full dark:bg-[#2B2D31] bg-[#F2F3F5]">
       <ServerHeader server={server} role={role} />
       <ScrollArea className="flex-1 px-3">
         <div className="mt-2">
           <ServerSearch
-          data={[
-            {
-              label: "Text Channels",
-              type: "channel",
-              data: textChannels?.map((channel)=>({
-                id: channel.id,
-                name: channel.name,
-                icon: iconMap[channel.type],
-              }))
-            },
-            {
-              label: "Voice Channels",
-              type: "channel",
-              data: audioChannels?.map((channel)=>({
-                id: channel.id,
-                name: channel.name,
-                icon: iconMap[channel.type],
-              }))
-            },
-            {
-              label: "Video Channels",
-              type: "channel",
-              data: videoChannels?.map((channel)=>({
-                id: channel.id,
-                name: channel.name,
-                icon: iconMap[channel.type],
-              }))
-            },
-            {
-              label: "Members",
-              type: "member",
-              data: members?.map((member)=>({
-                id: member.id,
-                name: member.profile.name,
-                icon: roleIconMap[member.role],
-              }))
-            },
-          ]
-          }
-          ></ServerSearch>
+            data={[
+              {
+                label: "Text Channels",
+                type: "channel",
+                data: textChannels?.map((channel) => ({
+                  id: channel.id,
+                  name: channel.name,
+                  icon: iconMap[channel.type],
+                })),
+              },
+              {
+                label: "Voice Channels",
+                type: "channel",
+                data: audioChannels?.map((channel) => ({
+                  id: channel.id,
+                  name: channel.name,
+                  icon: iconMap[channel.type],
+                })),
+              },
+              {
+                label: "Video Channels",
+                type: "channel",
+                data: videoChannels?.map((channel) => ({
+                  id: channel.id,
+                  name: channel.name,
+                  icon: iconMap[channel.type],
+                })),
+              },
+              {
+                label: "Members",
+                type: "member",
+                data: members?.map((member) => ({
+                  id: member.id,
+                  name: member.profile.name,
+                  icon: roleIconMap[member.role],
+                })),
+              },
+            ]}></ServerSearch>
         </div>
       </ScrollArea>
     </div>
