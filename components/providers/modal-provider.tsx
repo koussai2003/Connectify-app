729c5be70a1be0feb8c3c286ee7d3bbbed00192a
BlackFlame <chaouachiazizhasni@gmail.com>
2024-05-01 01:34:49 +0100

    EditChannel_test_kosaka

--- components/providers/modal-provider.tsx
+++ components/providers/modal-provider.tsx
@@ -8,6 +8,7 @@ import { MembersModal } from "../modals/members-modal";
 import { CreateChannelModal } from "@/components/modals/create-channel-modal";
 import { LeaveServerModal } from "@/components/modals/leave-server-modal";
 import { DeleteServerModal } from "@/components/modals/delete-server-modal";
+import { DeleteChannelModal } from "@/components/modals/;
 
 export const ModalProvider = () => {
   const [isMounted, setIsMounted] = useState(false);
@@ -26,6 +27,7 @@ export const ModalProvider = () => {
       <CreateChannelModal />
       <LeaveServerModal />
       <DeleteServerModal />
+      <DeleteChannelModal/>
     </>
   );
 };
