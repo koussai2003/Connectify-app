de3067fd31cdd1573ab52357ad4a94629ce7cae9
fawez9 <hattabifawez99@gmail.com>
2024-05-05 00:28:06 +0100

    quick modifying edit icon

--- components/server/server-channel.tsx
+++ components/server/server-channel.tsx
@@ -46,8 +46,8 @@ export const ServerChannel = ({ channel, server, role }: ServerChannelProps) => 
             <Edit
               onClick={(e) => onAction(e, "editChannel")}
               className="hidden group-hover:block w-4 h-4 text-zinc-500
-                        hover:text-rose-600 dark:text-zinc-400
-                        dark:hover:text-rose-400 transition"
+                        hover:text-zinc-600 dark:text-zinc-400
+                        dark:hover:text-zinc-300 transition"
             />
           </ActionTooltip>
           <ActionTooltip label="Delete">
